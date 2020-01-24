60d50d29e3542dedabf0206f3158846e77dcfa47
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-01-24 18:36:43 +1100

    Updated Bookmaker Formulas

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-const oddsPercentage = odds => {
-  let percentage = 1 / odds;
-
-  return (percentage * 100).toFixed(2);
-};
-
-console.log(oddsPercentage(1.22, 4.0));
-
 const bookmakerMargin = (odds1, odds2) => {
   let margin = 1 / odds1 + 1 / odds2;
 
@@ -13,3 +5,19 @@ const bookmakerMargin = (odds1, odds2) => {
 };
 
 console.log(bookmakerMargin(1.22, 4.0));
+
+const oddsPercentage = odds => {
+  let percentage = 1 / odds;
+
+  return (percentage * 100).toFixed(2);
+};
+
+console.log(oddsPercentage(1.22));
+
+const winningPercentage = (percentage, margin) => {
+  let odds = (1 - margin / 100) / (percentage / 100);
+
+  return odds;
+};
+
+console.log(winningPercentage(50, 5));
