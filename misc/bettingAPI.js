d2fb1b615a5777553912a43c0c9cbfe673887fa9
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-05-09 19:44:00 +1000

    Added JSON request

---
+++ misc/bettingAPI.js
@@ -0,0 +1,17 @@
+const oddsEl = document.getElementById('odds');
+let outputEl = document.getElementById('output');
+const APIkey = '0964ad4e3be969508766aef582e92012';
+
+// Get Odds
+function getOdds() {
+  fetch(`https://api.the-odds-api.com/v3/sports?apiKey=${APIkey}`)
+    .then((res) => res.json())
+    .then((data) =>
+      data.data.forEach((sport) => {
+        console.log(sport.key);
+      })
+    );
+}
+
+// Event Listeners
+oddsEl.addEventListener('click', getOdds);
