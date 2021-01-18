3de2147ec222b2b448d326ddb32d96c31e1972b5
Gareth Hind <garethjhind@gmail.com>
2021-01-18 22:47:08 +1100

    Added bookmaker logos

--- src/App.js
+++ src/App.js
@@ -9,16 +9,18 @@ function App() {
 
   const APIkey = '0964ad4e3be969508766aef582e92012';
 
+  let total = 0
   useEffect(() => {
     console.log("call");
     fetch(`https://api.the-odds-api.com/v3/sports?apiKey=${APIkey}`)
     .then((res) => res.json())
     .then((data) => {  
-      console.log(data.data);
-      setDataObject(data.data);
+      setDataObject([data.data])
+      total++
+      console.log(total);
       console.log(dataObject);
     });
-  }, []);
+  }, [dataObject]);
 
   // Output
   // let outputEl = document.getElementById('output');
@@ -31,7 +33,7 @@ function App() {
     <div className="app">
       <header className="app__header">
         <div className="u-margin-bottom-medium">
-          <h1><i class="fas fa-search-dollar"></i> Best Odds</h1>
+          <h1> Best  <i className="fas fa-search-dollar"></i>dds</h1>
         </div>
         <SearchBar />
         <Output />
