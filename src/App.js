b00009e60831a6c5272ca07fc0ad8a0ed0605253
Gareth Hind <garethjhind@gmail.com>
2021-01-20 23:22:08 +1100

    Updated betting app

--- src/App.js
+++ src/App.js
@@ -5,31 +5,23 @@ import { SearchBar } from './components/SearchBar/SearchBar'
 import { Output } from './components/Output/Output'
 
 function App() {
-  const [dataObject, setDataObject] = useState([])
+  const [dataObject, setDataObject] = useState()
 
   const APIkey = '0964ad4e3be969508766aef582e92012';
 
-  let total = 0
-  
   useEffect(() => {
-    console.log("call");
-    fetch(`https://api.the-odds-api.com/v3/sports?apiKey=${APIkey}`)
-    .then((res) => res.json())
-    .then((data) => {  
-      setDataObject([data.data])
-      total++
-      console.log(total);
-      console.log(dataObject);
-    });
-  }, []);
+    function getOdds() {
+      fetch(
+        `https://api.the-odds-api.com/v3/odds/?sport=upcoming&region=au&mkt=h2h&apiKey=${APIkey}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        });
+    }
+    getOdds()
+  }, [])
 
-  // Output
-  // let outputEl = document.getElementById('output');
-  // let tableEl = document.getElementById('table');
-  // let sportEl = document.getElementById('selectSport');
-  // let compEl = document.getElementById('selectComp');
-  // let marketEl = document.getElementById('selectMarket');
-  // let regionEl = document.getElementById('selectRegion');
 
   return (
     <div className="app">
